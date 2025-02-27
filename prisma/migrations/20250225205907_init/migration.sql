5787476f37694e83137543f1ef83425337ec4701
maxmalato-dell-3567 <max.malato@hotmail.com>
2025-02-27 07:58:34 -0300

    feat: alteração no formato de horas para PT-BR e tratamento de erro com o TRY CATCH

---
+++ prisma/migrations/20250225205907_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Feedback" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "comment" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Feedback_pkey" PRIMARY KEY ("id")
+);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import { PrismaClient } from '@prisma/client';
+import { format } from "date-fns"
+import { ptBR } from "date-fns/locale"
 
 const app = express();
 const prisma = new PrismaClient();
@@ -9,96 +11,129 @@ app.use(express.json());
 
 // Rota para listar todos os feedbacks
 app.get('/feedbacks', async (req, res) => {
-    const feedbacks = await prisma.feedback.findMany({
-        orderBy: {
-            createdAt: 'desc'
-        }
-    });
-    res.json(feedbacks);
+    try {
+        const feedbacks = await prisma.feedback.findMany({
+            orderBy: {
+                createdAt: 'asc'
+            }
+        });
+
+        const formattedFeedbacks = feedbacks.map(feedback => {
+            return {
+                ...feedback,
+                createdAt: format(new Date(feedback.createdAt), "dd 'de' MMMM 'de' yyyy HH:mm:ss", { locale: ptBR }),
+                updatedAt: feedback.updatedAt ? format(new Date(feedback.updatedAt), "dd 'de' MMMM 'de' yyyy HH:mm:ss", { locale: ptBR }) : null
+            }
+        });
+
+        res.json(formattedFeedbacks);
+    } catch (error) {
+        res.status(500).json({
+            error: 'Não foi possível listar os feedbacks.'
+        });
+    }
 });
 
 // Rota para criar um feedback
 app.post('/feedbacks', async (req, res) => {
-    const { name, comment } = req.body;
+    try {
+        const { name, comment } = req.body;
 
-    if (!name || !comment) {
-        return res.status(400).json({
-            error: 'Nome e comentário são obrigatórios.'
+        if (!name || !comment) {
+            return res.status(400).json({
+                error: 'Nome e comentário são obrigatórios.'
+            });
+        }
+
+        const newFeedback = await prisma.feedback.create({
+            data: {
+                name,
+                comment
+            }
+        });
+        res.status(201).json(newFeedback);
+    } catch (error) {
+        res.status(500).json({
+            error: 'Não foi possível criar o feedback.'
         });
     }
-
-    const newFeedback = await prisma.feedback.create({
-        data: {
-            name,
-            comment
-        }
-    });
-    res.status(201).json(newFeedback);
 });
 
 // Rota para atualizar um feedback
 app.put('/feedbacks/:id', async (req, res) => {
-    const { id } = req.params;
-    const { name, comment } = req.body;
+    try {
+        const { id } = req.params;
+        const { name, comment } = req.body;
 
-    // Encontra o feedback existente pelo ID
-    const existingFeedback = await prisma.feedback.findUnique({
-        where: {
-            id: Number(id)
+        // Encontra o feedback existente pelo ID
+        const existingFeedback = await prisma.feedback.findUnique({
+            where: {
+                id: Number(id)
+            }
+        });
+
+        // Verifica se o feedback existe
+        if (!existingFeedback) {
+            return res.status(404).json({
+                error: 'Feedback não encontrado.'
+            });
         }
-    });
 
-    // Verifica se o feedback existe
-    if (!existingFeedback) {
-        return res.status(404).json({
-            error: 'Feedback não encontrado.'
+        // Verifica se o nome do feedback é o mesmo do usuário que está tentando editar
+        if (existingFeedback.name !== name) {
+            return res.status(403).json({
+                error: 'Você só pode editar os seus próprios comentários.'
+            })
+        }
+
+        const updateFeedback = await prisma.feedback.update({
+            where: {
+                id: Number(id)
+            },
+            data: {
+                comment
+            }
+        });
+        res.json(updateFeedback);
+    } catch (error) {
+        res.status(500).json({
+            error: 'Não foi possível atualizar o feedback.'
         });
     }
-
-    // Verifica se o nome do feedback é o mesmo do usuário que está tentando editar
-    if (existingFeedback.name !== name) {
-        return res.status(403).json({
-            error: 'Você só pode editar os seus próprios comentários.'
-        })
-    }
-
-    const updateFeedback = await prisma.feedback.update({
-        where: {
-            id: Number(id)
-        },
-        data: {
-            comment
-        }
-    });
-    res.json(updateFeedback);
 });
 
 // Rota para deletar um feedback
 app.delete('/feedbacks/:id', async (req, res) => {
-    const { id } = req.params;
+    try {
+        const { id } = req.params;
 
-    // Encontra o feedback existente pelo ID
-    const existingFeedback = await prisma.feedback.findUnique({
-        where: {
-            id: Number(id)
+        // Encontra o feedback existente pelo ID
+        const existingFeedback = await prisma.feedback.findUnique({
+            where: {
+                id: Number(id)
+            }
+        });
+
+        // Verifica se o feedback existe
+        if (!existingFeedback) {
+            return res.status(404).json({
+                error: 'Comentário não encontrado.'
+            });
         }
-    });
 
-    // Verifica se o feedback existe
-    if (!existingFeedback) {
-        return res.status(404).json({
-            error: 'Comentário não encontrado.'
+        await prisma.feedback.delete({
+            where: {
+                id: Number(id)
+            }
+        });
+        res.json({
+            message: 'Comentário deletado com sucesso.'
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: 'Não foi possível deletar o feedback.'
         });
     }
-
-    await prisma.feedback.delete({
-        where: {
-            id: Number(id)
-        }
-    });
-    res.json({
-        message: 'Comentário deletado com sucesso.'
-    });
 });
 
 // Inicia o servidor na porta 5000
